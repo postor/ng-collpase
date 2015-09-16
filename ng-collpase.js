18a9d0381890fc9b1c5fc71579d0d39e28c96f8a
junjun <postor@gmail.com>
2015-09-16 15:44:45 +0800

    Update ng-collpase.js
    改为使用controller as实现

--- ng-collpase.js
+++ ng-collpase.js
@@ -1,66 +1,68 @@
-// Code goes here
 
 // declare the app with no dependencies
-angular.module('collpase', []).factory('CollpaseData', function(){
-    // I know this doesn't work, but what will?
-    var list = {};
-    
-    var newGroup = function(group,option){
-      list[group] = {
-        openCount: 0,
-        items:{},
-        option: angular.extend(
-          {
-            current: null,
-            forceOneOpen: 0,
-            forceOtherClose: 1,
-          },
-          option),
-        init:function(){
-          this.openCount = 0;
-          if(this.option.current===null) return;  
-          for(var j in this.option.current){
-            this.items[j] = this.option.current[j];
-            if(this.option.current[j])this.openCount++;
-          }
-        },
-        select: function(key){
-          console.log(key+' selected');
-          if(this.items[key]){
-            //已经展开
-            if(this.option.forceOneOpen) return;
-            
-            this.openCount--;
-            this.items[key] = 0;
-          }else{
-            //关闭
-            if(this.option.forceOtherClose){
-              this.openCount = 0;
-              for(var k in this.items){
-                this.items[k] = 0;
-              }
-            }
-            //展开
-            this.openCount++;
-            this.items[key] = 1;
-          }
-        }
-      };
-      list[group].init();
-    };
-    
-    return {
-      newGroup:newGroup,
-      list:list
-    };
-}).directive('ngCollpase', function($compile)
+angular.module('collpase', []).directive('ngCollpase', function($compile)
 { 
+  function CollpaseController (){
+    
+    
+  };
+  
+  var list = {};
+    
+  var newGroup = function(group,option){
+    list[group] = {
+      openCount: 0,
+      items:{},
+      option: angular.extend(
+        {
+          current: null,
+          forceOneOpen: 0,
+          forceOtherClose: 1,
+        },
+        option),
+      init:function(){
+        this.openCount = 0;
+        if(this.option.current===null) return;  
+        for(var j in this.option.current){
+          this.items[j] = this.option.current[j];
+          if(this.option.current[j])this.openCount++;
+        }
+      },
+      select: function(key){
+        if(this.items[key]){
+          //已经展开
+          if(this.option.forceOneOpen) return;
+          
+          this.openCount--;
+          this.items[key] = 0;
+        }else{
+          //关闭
+          if(this.option.forceOtherClose){
+            this.openCount = 0;
+            for(var k in this.items){
+              this.items[k] = 0;
+            }
+          }
+          //展开
+          this.openCount++;
+          this.items[key] = 1;
+        }
+      }
+    };
+    list[group].init();
+  };
+  
+  CollpaseController.prototype = {
+    newGroup:newGroup,
+    list:list
+  };
+  
+  
   
   return {
       restrict: 'A',
-      controller: ['$scope','CollpaseData', function($scope,CollpaseData){
-        if(!$scope.collpaseData) $scope.collpaseData = CollpaseData;
-      }],
+      controller: CollpaseController,
+      controllerAs:'collpaseData',
       link: function(scope, el, attrs, controller) {
         var option = {};
         if(attrs.ngCollpaseOption){
@@ -82,7 +84,6 @@ angular.module('collpase', []).factory('CollpaseData', function(){
         var parent = tAttrs['ngCollpaseParent'];
         parent = scope.$eval(parent);
         var prefixedParent = parent;
-        console.log([parent,key]);
         
         tElement.attr('ng-class',"{'collpase-target-open':collpaseData.list['"
         +prefixedParent+"'].items['"+prefixedKey
@@ -111,7 +112,6 @@ angular.module('collpase', []).factory('CollpaseData', function(){
         var parent = tAttrs['ngCollpaseParent'];
         parent = scope.$eval(parent);
         var prefixedParent = parent;
-        console.log([parent,key]);
         
         tElement.attr('ng-class',"{'collpase-href-open':collpaseData.list['"
         +prefixedParent+"'].items['"+prefixedKey
